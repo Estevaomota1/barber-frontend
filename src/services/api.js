0f539c371269f925066850eadd8243dee40337f2
ESTEVAO AUGUSTO MOTA DOS SANTOS <124156394+Estevaomota1@users.noreply.github.com>
2026-05-08 16:30:20 -0300

    fix baseURL hardcoded

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000/api',
+  baseURL: 'https://barber-saas-ggo4.onrender.com/api',
 })
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token')
